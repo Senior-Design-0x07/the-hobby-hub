2ee55412c9a6a403a4ead6c8042887932ab15eda
Ivendrakeo <dravenschilling@gmail.com>
2020-12-05 20:41:29 +0000

    add beaglebone software update to the setup script

--- setup.sh
+++ setup.sh
@@ -7,17 +7,34 @@ echo ""
 echo ""
 echo "Running hobby-hub setup script."
 echo "This may take a few moments..."
+echo""
+
+echo "||||||||||||||||||||||||||||||||"
+echo "||| updating device packages |||"
+echo "||||||||||||||||||||||||||||||||"
+echo ""
+sudo apt-get update
+sudo apt-get upgrade
 
 mkdir -p /etc/hobby-hub/
 
-echo "Processing sub-scripts:"
+
+echo "||||||||||||||||||||||||||||||||"
+echo "||| processing sub-scripts  ||||"
+echo "||||||||||||||||||||||||||||||||"
+echo""
 for f in scripts/*.sh
 do
   echo "$f"
   sh $f
 done
+echo"" 
 
-echo "Setup Complete! The device will now restart..."
+echo "||||||||||||||||||||||||||||||||"
+echo "||||||| setup complete! ||||||||"
+echo "||||||| restarting...   ||||||||"
+echo "||||||||||||||||||||||||||||||||"
 echo ""
 sleep 3
-#reboot
+
+#sudo reboot
