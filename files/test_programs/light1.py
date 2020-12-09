73bf8787bdd3cc90078abc1314ae19ec07935081
Demo User <debian@beaglebone.localdomain>
2020-12-09 19:08:23 +0000

    Write two user test files in python to control the USR0 and USR1 LEDs on the beaglebone board. This approach uses the Adafruit_BBIO python library instead of the file path that the C++ test programs use

---
+++ files/test_programs/light1.py
@@ -0,0 +1,13 @@
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+USR1_LED = "USR1"
+delay = 0.25
+GPIO.setup(USR1_LED, GPIO.OUT)
+
+while (True):
+    GPIO.output(USR1_LED, GPIO.HIGH)
+    time.sleep(delay)
+
+    GPIO.output(USR1_LED, GPIO.LOW)
+    time.sleep(delay)
