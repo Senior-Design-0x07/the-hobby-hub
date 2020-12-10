3b5bd8451e58c482be38d76ff5228d9d25779e74
Rock Boynton <boyntonrl@msoe.edu>
2020-12-10 06:13:04 +0000

    Initial workings for the program manager Still trying to decide whether I will go with python or c for this. It's turning out to be a little more challenging than I thought it would be.

---
+++ parallize.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+import subprocess
+from pathlib import Path
+
+
+def main(arguments):
+    """Takes a directory containing user programs and runs them as seperate processes
+
+    Only runs .c, .cpp, and .py files for now
+    For .c files:
+        Uses a makefile if in the directory, or gcc to compile
+
+
+    Args:
+        arguments (string): [description]
+    """
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('program_directory', help="Directory containing user programs to run", type=dir_path)
+    # TODO add output file for each program in a specified output directory
+
+    args = parser.parse_args(arguments)
+
+    # get directory of programs
+    program_dir = Path(args.program_directory)
+
+    # extract programs
+    py_files = list(program_dir.glob('**/*.py'))
+    c_files = list(program_dir.glob('**/*.c'))
+    cpp_files = list(program_dir.glob('**/*.cpp'))
+
+    # compile c files
+
+    programs = py_files + c_files + cpp_files
+
+    for program in py_files:
+        subprocess.run(['python3', program, '&'])
+
+
+
+    # iterate over each of the .c and .py files in the directory
+
+    # kickoff each program
+
+    # exit
+
+    print(py_files)
+    print(c_files)
+
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
