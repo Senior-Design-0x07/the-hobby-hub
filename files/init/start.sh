f30eca6f302b233ab2f59925a920ec848819c3f1
Rock Boynton <boyntonrl@msoe.edu>
2020-12-15 05:40:22 +0000

    Start program manager on startup

--- files/init/start.sh
+++ files/init/start.sh
@@ -4,4 +4,8 @@
 # act as an entry point to startup the rest of the hobby-hub utilities (as needed).
 
 # test message - viewable in $journalctl
+
+# Start program manager
+sudo python3 /etc/hobby-hub/program_manager.py /etc/hobby-hub/test_programs/ > /etc/hobby-hub/out/running_programs.txt
+
 echo "hobby-hub startup sucessful!"
