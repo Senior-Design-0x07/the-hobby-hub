72f78fa9b9e3cb4692003eebf478c42b9c6940e8
Rock Boynton <rock.boynton@yahoo.com>
2020-12-15 00:09:29 -0600

    Start program manager before server

--- files/init/start.sh
+++ files/init/start.sh
@@ -3,13 +3,13 @@
 # This program provides a script that is run upon device startup that will
 # act as an entry point to startup the rest of the hobby-hub utilities (as needed).
 
+# Start program manager
+sudo python3 /etc/hobby-hub/program_manager.py /etc/hobby-hub/test_programs/ > /etc/hobby-hub/out/running_programs.txt
+
 #Runs Flask Backend Server on startup
 echo " || Starting Web Server ||"
 python /etc/hobby-hub/web/backend/start-server.py
 
 # test message - viewable in $journalctl
-
-# Start program manager
-sudo python3 /etc/hobby-hub/program_manager.py /etc/hobby-hub/test_programs/ > /etc/hobby-hub/out/running_programs.txt
 
 echo "hobby-hub startup sucessful!"
