28086d00dc25a55f716fa1f0a0e5d3b2ffa373b1
Rock Boynton <boyntonrl@msoe.edu>
2020-12-15 05:21:41 +0000

    Copy program manager to system config on init

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# Hobby Hub system configuration directory
 mkdir -p /etc/hobby-hub/
+
+# Startup script
 cp files/init/start.sh /etc/hobby-hub/
+
+# Program manager
+mkdir -p /etc/hobby-hub/out/ # contains output of program manager
+cp files/src/program_manager.py /etc/hobby-hub/ # program manager itself
