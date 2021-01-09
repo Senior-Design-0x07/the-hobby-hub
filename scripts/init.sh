cc98e4a54ead57e853b9a85fcedd7f7c107024ee
Ivendrakeo <dravenschilling@gmail.com>
2021-01-09 22:24:09 +0000

    add dev user. install additional pre-req modules

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,3 +9,9 @@ cp files/init/start.sh /etc/hobby-hub/
 # Program manager
 mkdir -p /etc/hobby-hub/out/ # contains output of program manager
 cp files/src/program_manager.py /etc/hobby-hub/ # program manager itself
+
+# Create dev user & group. add new user to required groups.
+sudo useradd --comment "Development User" --user-group --create-home --shell /bin/bash dev
+sudo usermod --append --groups dev debian 
+sudo usermod --append --groups dialout,users,systemd-journal,bluetooth,i2c,gpio,pwm,spi,iio 
+echo -e "dev\ndev" | sudo passwd dev
--- setup.sh
+++ setup.sh
@@ -55,6 +55,7 @@ then
   echo ""
   sudo apt-get update
   sudo apt-get upgrade
+  sudo apt-get install openssh-server
 fi
 
 mkdir -p /etc/hobby-hub/
