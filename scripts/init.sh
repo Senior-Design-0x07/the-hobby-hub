20a311bcb783711435dbf32c13c13435e53c3e69
Development User <dev@beaglebone.localdomain>
2021-01-26 19:43:54 +0000

    add command for listing programs

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,4 +8,5 @@ cp files/init/start.sh /etc/hobby-hub/
 
 # Program manager
 mkdir -p /etc/hobby-hub/out/ # contains output of program manager
+touch /etc/hobby-hub/out/paused_programs.txt
 cp files/src/program_manager.py /etc/hobby-hub/ # program manager itself
