3d3d000c0492004fdfa47f980edb44a9a4934e2a
Andrew Iliescu <iliescuandrew@gmail.com>
2021-02-10 01:49:19 +0000

    Fixed shell files so that it now has a scan wifi feature

--- files/init/start.sh
+++ files/init/start.sh
@@ -3,6 +3,9 @@
 # This program provides a script that is run upon device startup that will
 # act as an entry point to startup the rest of the hobby-hub utilities (as needed).
 
+# Runs the script to get/maintain an ip address
+sudo /etc/hobby-hub/cron/wifi_scripts.sh
+
 # Start program manager
 sudo python3 /etc/hobby-hub/program_manager.py /etc/hobby-hub/test_programs/ > /etc/hobby-hub/out/running_programs.txt
 
--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 sudo mkdir /etc/hobby-hub/cron
-
-echo "sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf -D nl80211 \nsudo dhclient wlan0 \nsleep 10" | sudo tee /etc/hobby-hub/cron/wifi_scripts.sh
-
 sudo rm -r /etc/crontab
 sudo cp files/cron/crontab /etc/crontab
+sudo cp files/cron/wif_scripts.sh /etc/hobby-hub/cron
