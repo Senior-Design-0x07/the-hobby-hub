46f00cb07cab8d2082e3913dc19faf01b2f2eb70
Anthony Bartman <anthonyb3699@yahoo.com>
2021-03-12 03:44:54 +0000

    Finished HTTP Post request

--- files/web/backend/resources/Pin_Manager.py
+++ files/web/backend/resources/Pin_Manager.py
@@ -1,11 +1,22 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import json
 
+parser = reqparse.RequestParser()
+parser.add_argument("ssid", type=str, help="Task is required DUDE.", required=True)
+parser.add_argument("password", type=str, help="Task is required BRO.", required=True)
+
 class Pin_Manager(Resource):
+
     def get(self):
         f = open("/etc/hobby-hub/pin_mapping.json", "r")
         pin_mapping = json.load(f)
         f.close()
         return pin_mapping
     def post(self):
-        return {"num_backend": 5,"string_backend": "Hello From Flask in Example.py"}
+        args = parser.parse_args()
+
+        print("REQUEST: ")
+        print("   SSID: " + args["ssid"])
+        print("   Password: " + args["password"])
+        # Null in python
+        return None
