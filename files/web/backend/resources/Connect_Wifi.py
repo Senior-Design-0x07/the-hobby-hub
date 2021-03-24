42633580ff99d31cac5baa717ead0f17090cdf8c
Demo User <debian@beaglebone.localdomain>
2021-03-24 17:30:35 +0000

    Added new necessary files for scanning networks and connecting to the desired network from the web UI

--- files/web/backend/resources/Connect_Wifi.py
+++ files/web/backend/resources/Connect_Wifi.py
@@ -1,14 +1,9 @@
 from flask_restful import Resource
 
-class Scan_Wifi(Resource):
+class Connect_Wifi(Resource):
     def get(self):
         # subprocess.run([sys.executable, "/~/the-hobby-hub/scripts/scan_network.sh"])
 
-        f = open("/home/debian/the-hobby-hub/files/wifi_scan/SSID.txt")
-
-        scanned_networks = f.readlines()
-        f.close()
-        return scanned_networks
     
     def post(self):
         return {"num_backend": 5,"string_backend": "Hello From Flask in Example.py"}
