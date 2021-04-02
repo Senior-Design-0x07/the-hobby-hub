c48e1a16de814df3e190d7d9ae55a6338a465a23
Demo User <debian@beaglebone.localdomain>
2021-04-02 18:57:14 +0000

    Updated Get_IP script

--- files/web/backend/resources/Get_IP.py
+++ files/web/backend/resources/Get_IP.py
@@ -1,9 +1,10 @@
 import os
 
 class Get_IP:
-    os.system("hostname -I | awk '{print $3}' | tee /etc/hobby-hub/wifi_scan/ip.txt")
-    f = open("/etc/hobby-hub/wifi_scan/ip.txt", "r")
-    board_ip = f.read()
-    f.close()
-    return board_ip
+    def return_ip: 
+        os.system("hostname -I | awk '{print $3}' | tee /etc/hobby-hub/wifi_scan/ip.txt")
+        f = open("/etc/hobby-hub/wifi_scan/ip.txt", "r")
+        board_ip = f.read()
+        f.close()
+        return board_ip
 
