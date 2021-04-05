4d32fe1e0310f14443e43deb369c45ca642b24a1
Demo User <debian@beaglebone.localdomain>
2021-04-05 03:12:52 +0000

    Fixed silly mistakes from testing. Supports pause and continue for programs well in current state. Cannot stop a singular program yet

--- files/web/backend/resources/Program_Command.py
+++ files/web/backend/resources/Program_Command.py
@@ -11,16 +11,20 @@ class Program_Command(Resource):
         args = parser.parse_args()
         if(args["command"] == 'pause_program'):
             # pause the supplied program (provided program is running)
-            os.system("sudo hobby-hub -p " + args["program"])
+            program_command = "sudo hobby-hub -p " + args["program"]
+            os.system(program_command)
 
         elif(args["command"] == 'continue_program'):
             # continue the supplied program (provided program is paused)
-            os.system("sudo hobby-hub -c " + args["program"])
+            program_command = "sudo hobby-hub -c " + args["program"]
+            os.system(program_command)
 
         elif(args["command"] == 'stop_program'):
             # stop the supplied program completely (provided program is running)
+            program_command = "sudo hobby-hub -s " + args["program"]
             os.system("sudo hobby-hub -s " + args["program"])
 
         elif(args["command"] == 'start_program'):
             # start the supplied program
-            os.system("sudo hobby-hub -t " + args["program"])
+            program_command = "sudo hobby-hub -t " + args["program"]
+            os.system(program_command)
