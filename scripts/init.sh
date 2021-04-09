8a99775550bf466fec57f227cda9fa8b695b04e0
Rock Boynton <boyntonrl@msoe.edu>
2021-04-09 00:11:56 +0000

    Stop appending to pin config
    Instead rewrite it with empty JSON syntax. There was issues before
    where extraneous brackets were appended after running the init script
    again.

--- scripts/init.sh
+++ scripts/init.sh
@@ -48,7 +48,7 @@ sudo cp files/system/sshd_config /etc/ssh/
 sudo cp files/system/issue.net /etc/
 
 # Pin mapping configuration
-echo "{}" | sudo tee -a /etc/hobby-hub/pin_mapping.json # creates empty, but valid JSON file
+echo "{}" | sudo tee /etc/hobby-hub/pin_mapping.json # creates empty, but valid JSON file
 chmod og=rw /etc/hobby-hub/pin_mapping.json
 cp files/src/pin_manager.py /etc/hobby-hub/ # pin manager itself
 sudo python3 -m pip install /home/debian/the-hobby-hub/files/src/hobby_hub_utils # install package for user dev
