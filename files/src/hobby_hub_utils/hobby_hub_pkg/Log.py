5953896a733cf165b478936fe3bb0293fb1b8d82
Ivendrakeo <DravenSchilling@gmail.com>
2021-04-14 11:55:26 -0500

    add datetime before log message

--- files/src/hobby_hub_utils/hobby_hub_pkg/Log.py
+++ files/src/hobby_hub_utils/hobby_hub_pkg/Log.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Log:
 
     __log_file = None
@@ -11,7 +14,9 @@ class Log:
             raise Exception('invalid log file')
 
     def write(self, message):
-        self.__log_file.write(message + "\n")
+        ct = datetime.datetime.now()
+        time = ct.strftime("%x") + " " + ct.strftime("%X")
+        self.__log_file.write(time + message + "\n")
         self.__log_file.flush()
 
     def clear(self):
