d25ed7f404fd43d4360d356661a23330e85e25bf
Demo User <debian@beaglebone.localdomain>
2021-04-15 20:26:52 +0000

    Method created that will issue a ping command and check to see if the command was successful to determine if internet connection was established

--- files/web/backend/resources/Get_IP.py
+++ files/web/backend/resources/Get_IP.py
@@ -1,10 +1,17 @@
 import os
 
 class Get_IP:
-    def return_ip: 
+    def return_ip(): 
         os.system("hostname -I | awk '{print $3}' | tee /etc/hobby-hub/wifi_scan/ip.txt")
         f = open("/etc/hobby-hub/wifi_scan/ip.txt", "r")
         board_ip = f.read()
         f.close()
         return board_ip
+    
+    def check_ping():
+        response = os.system("ping -4 www.google.com")
+        if response == 0:
+            return True
+        else:
+            return False
 
